275cd5ba3b2db59b40b185d633fdba5473d33c87
Kathleen Eife <keife218@icloud.com>
2018-07-30 14:23:54 +0000

    automatically backed up by learn

--- amazon_transaction.rb
+++ amazon_transaction.rb
@@ -1,4 +1,8 @@
 class amazon
-  def 
-  end
+ def initialize(product, price, amazon_prime, print_receipt)
+   @product = product
+   @price = price
+   @amazon_prime = amazon_prime
+   @print_receipt = print_receipt
+ end
 end 

73862e96aa8d24fd262ce31c55c5ab9354742bc8
Kathleen Eife <keife218@icloud.com>
2018-07-30 14:31:33 +0000

    automatically backed up by learn

--- amazon_transaction.rb
+++ amazon_transaction.rb
@@ -1,5 +1,5 @@
 class amazon
-  attr_accessor :product, :price
+  attr_accessor :product, :price, :amazon_prime, :print_receipt
   def initialize(product, price, amazon_prime, print_receipt)
     @product = product
     @price = price
